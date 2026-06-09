c00969b4d8c50a17c0614766c53e4117d7ae61ac
DLog <126749897+FPTSTUDEN@users.noreply.github.com>
2026-06-09 12:08:59 +0700

    homework w1

---
+++ src/main/java/task2.java
@@ -0,0 +1,9 @@
+public class task2 {
+    public static void main(String[] args) {
+        System.out.println("    *\r\n" + //
+                        "   ***\r\n" + //
+                        "  *****\r\n" + //
+                        " *******\r\n" + //
+                        " ");
+    }
+}
